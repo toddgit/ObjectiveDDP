0213e8b587d421100b8f62c523a3963210e7c517
Martin Ceperley <martin@ceperley.com>
2014-07-03 15:59:11 -0400

    Added supportedVersions

--- ObjectiveDDP/ObjectiveDDP.m
+++ ObjectiveDDP/ObjectiveDDP.m
@@ -32,7 +32,7 @@
 //  version: string (the proposed protocol version)
 //  support: array of strings (protocol versions supported by the client, in order of preference)
 - (void)connectWithSession:(NSString *)session version:(NSString *)version support:(NSString *)support {
-    NSDictionary *fields = @{@"msg": @"connect", @"version": version};
+    NSDictionary *fields = @{@"msg": @"connect", @"version": version, @"support": support};
     NSString *json = [self _buildJSONWithFields:fields parameters:nil];
     [self.webSocket send:json];
 }
